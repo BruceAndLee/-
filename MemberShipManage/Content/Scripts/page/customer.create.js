b157c34f369f03e80d5c95ea75f61475107eb58c
asus <asus@DESKTOP-OHVP0KQ>
2018-12-20 00:21:42 +0800

    update

---
+++ MemberShipManage/Content/Scripts/page/customer.create.js
@@ -0,0 +1,96 @@
+﻿$(function () {
+    $('#customerform').bootstrapValidator({
+        message: 'This value is not valid',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            userNo: {
+                validators: {
+                    notEmpty: {
+                        message: '用户名不能为空'
+                    },
+                    regexp: {
+                        regexp: /^1[3|5|8]{1}[0-9]{9}$/,///^\d{11}$/,
+                        message: '请输入11位手机号.'
+                    }
+                }
+            },
+            passWord: {
+                validators: {
+                    notEmpty: {
+                        message: '密码不能为空'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 10,
+                        message: '密码长度必须在6到10之间'
+                    },
+                    different: {//不能和用户名相同
+                        field: 'userNo',
+                        message: '不能和用户名相同'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z0-9\.]+$/,
+                        message: '只能是数字或字母'
+                    }
+                }
+            },
+            userRePwd: {
+                validators: {
+                    notEmpty: {
+                        message: '确认密码不能为空'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 10,
+                        message: '确认密码必须在6到10之间'
+                    },
+                    identical: {//相同
+                        field: 'passWord',
+                        message: '两次密码不一致'
+                    },
+                    regexp: {//匹配规则
+                        regexp: /^[a-zA-Z0-9\.]+$/,
+                        message: '只能是数字或字母'
+                    }
+                }
+            },
+            sex: {
+                validators: {
+                    notEmpty: {
+                        message: '请选择性别'
+                    }
+                }
+            },
+            name: {
+                validators: {
+                    stringLength: {
+                        min: 1,
+                        max: 10,
+                        message: ''
+                    }
+                }
+            }
+        }
+    }).on('success.form.bv', function (e) {//点击提交之后
+        e.preventDefault();
+        var $form = $(e.target);
+        $.post($form.attr('action'), $form.serialize(), function (result) {
+            if (result.IsSuc) {
+                alert("注册成功！");
+                clearform();
+            }
+        });
+        });
+
+    function clearform() {
+        $('#userNo').val('');
+        $('#userPwd').val('');
+        $('#userRePwd').val('');
+        $('#userSex').val('');
+        $('#name').val('');
+    }
+});
---
+++ MemberShipManage/Content/Scripts/page/customer.update.js
@@ -0,0 +1,69 @@
+﻿$(function () {
+    registerValidator();
+    $('#updateModal').on('hide.bs.modal',
+        function () {
+            $("#customerupdate").data('bootstrapValidator').destroy();
+            $('#customerupdate').data('bootstrapValidator', null);
+            registerValidator();
+
+            $('#userNo').val('');
+            $('#userSex').val('');
+            $('#name').val('');
+        });
+
+    function registerValidator() {
+        $('#customerupdate').bootstrapValidator({
+            message: 'This value is not valid',
+            feedbackIcons: {
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+            fields: {
+                userNo: {
+                    validators: {
+                        notEmpty: {
+                            message: '用户名不能为空'
+                        },
+                        regexp: {
+                            regexp: /^1[3|5|8]{1}[0-9]{9}$/,///^\d{11}$/,
+                            message: '请输入11位手机号.'
+                        }
+                    }
+                },
+                sex: {
+                    validators: {
+                        notEmpty: {
+                            message: '请选择性别'
+                        }
+                    }
+                },
+                name: {
+                    validators: {
+                        stringLength: {
+                            min: 1,
+                            max: 10,
+                            message: ''
+                        }
+                    }
+                }
+            }
+        }).on('success.form.bv', function (e) {//点击提交之后
+            e.preventDefault();
+            var $form = $(e.target);
+            $.post($form.attr('action'), $form.serialize(), function (result) {
+                if (result.IsSuc) {
+                    alert("修改成功！");
+                    clearform();
+                }
+            });
+        });
+    }
+
+    function clearform() {
+        $('#userNo').val('');
+        $('#userPwd').val('');
+        $('#userRePwd').val('');
+        $('#name').val('');
+    }
+});
