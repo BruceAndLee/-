b16001124d3a5b6f67176b601b85aeb8336a617f
lilei1986abc <lilei1986abc@163.com>
2018-12-25 23:44:50 +0800

    update

---
+++ MemberShipManage.Domain/Entity/UserPwdUpdateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberShipManage.Domain.Entity
+{
+    public class UserPwdUpdateRequest
+    {
+        public string OrgPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
--- MemberShipManage/Controllers/UsersController.cs
+++ MemberShipManage/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 ﻿using MemberShipManage.Domain;
+using MemberShipManage.Domain.Entity;
 using MemberShipManage.Infrastructure.Base;
 using MemberShipManage.Infrastructure.Filter;
 using MemberShipManage.Infrastructure.RestAPI;
 using MemberShipManage.Service.UserManage;
+using MemberShipManage.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,15 +22,35 @@ namespace MemberShipManage.Controllers
             this.userService = userService;
         }
 
-        public JsonResult UpdatePassword(Users user)
+        [HttpGet]
+        public ViewResult Password()
         {
-            var userDb = userService.GetUser(user.UserNo);
+            return View();
+        }
+
+        [HttpPut]
+        public JsonResult UpdatePassword(UserPwdUpdateRequest request)
+        {
+            var currentUser = Session["User"] as Users;
+            if (currentUser == null)
+            {
+                return JsonResult(new APIBaseResponse(false, "UM_004"));
+            }
+
+            var userDb = userService.GetUser(currentUser.UserNo);
             if (userDb == null)
             {
                 return JsonResult(new APIBaseResponse { IsSuccess = false, ResponseCode = "UM_003" });
             }
 
-            userService.UpdatePassword(user);
+            if (Cryptor.Decrypt(userDb.Password) != request.OrgPassword)
+            {
+                return JsonResult(new APIBaseResponse(false, "UM_005"));
+            }
+
+            userDb.Password = Cryptor.Encrypt(request.NewPassword.Trim());
+            userService.UpdatePassword(userDb);
+            Session["User"] = null;
             return SuccessJsonResult();
         }
     }
