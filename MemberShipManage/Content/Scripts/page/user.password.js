ac2b7fc4910fd22d57d9df98ee542363989b270e
lilei1986abc <lilei1986abc@163.com>
2018-12-26 23:47:29 +0800

    update

---
+++ MemberShipManage/Content/Scripts/page/user.password.js
@@ -0,0 +1,60 @@
+﻿$(function () {
+    $('#updatePwdform').bootstrapValidator({
+        message: 'This value is not valid',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            orgPassword: {
+                validators: {
+                    notEmpty: {
+                        message: '原密码不能为空'
+                    }
+                }
+            },
+            newPassword: {
+                validators: {
+                    notEmpty: {
+                        message: '新密码不能为空'
+                    }, regexp: {
+                        regexp: /^[a-zA-Z0-9\.]+$/,
+                        message: '只能是数字或字母'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 10,
+                        message: '密码长度必须在6到10之间'
+                    }
+                }
+            },
+            rePassword: {
+                validators: {
+                    notEmpty: {
+                        message: '重复密码不能为空'
+                    }
+                }
+            }
+        }
+    }).on('success.form.bv', function (e) {//点击提交之后
+        e.preventDefault();
+        $.ajax({
+            url: $form.attr('action'),
+            type: 'put',
+            data: {
+                OrgPassword: $('#orgPassword').val(),
+                NewPassword: $('#newPassword').val()
+            },
+            success: function (result) {
+                if (result.IsSuc) {
+                    alert("修改成功，请重新登录！");
+                    window.location.href = 'Home/Login';
+                }
+                else {
+                    alert(data.Msg);
+                }
+            }
+        });
+    });
+});
