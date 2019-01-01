e7c75eac527ae0bb6f5332fca7e0c9fe788d5e53
asus <asus@DESKTOP-OHVP0KQ>
2019-01-01 19:04:22 +0800

    update

--- MemberShipManage/Global.asax.cs
+++ MemberShipManage/Global.asax.cs
@@ -9,6 +9,7 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using System.Reflection;
 using MemberShipManage.Models;
+using MemberShipManage.Utility;
 
 namespace MemberShipManage
 {
@@ -27,6 +28,12 @@ namespace MemberShipManage
 
             Singleton<IAppStartManager>.Instance.Initialize(Assembly.GetExecutingAssembly());
             AutoMapperManager.InitMapperCollection();
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            LogHelper.Error(e.ToString());
         }
     }
 }
